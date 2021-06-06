f6f4775b7c2046a4f32694a42d0d26b5416d0f64
stanislavglazko <stanislavglazko@MacBook-Air-Apple.local>
2021-06-06 22:13:46 +0300

    base logic and tests

---
+++ linked_list/double_linked_list.py
@@ -0,0 +1,134 @@
+class Element:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def add_at_tail(self, val):
+        new_element = Element(val)
+        if not self.tail:
+            self.tail = new_element
+            self.head = new_element
+            self.length += 1
+            return
+        new_element.prev = self.tail
+        new_element.next = None
+        self.tail.next = new_element
+        self.tail = new_element
+        self.length += 1
+
+    def add_at_head(self, val):
+        new_element = Element(val)
+        if not self.head:
+            self.head = new_element
+            self.tail = new_element
+            self.length += 1
+            return
+        new_element.next = self.head
+        new_element.prev = None
+        self.head.prev = new_element
+        self.head = new_element
+        self.length += 1
+
+    def add_at_index(self, index, val):
+        if index == self.length:
+            self.add_at_tail(val)
+            return
+        elif index == 0:
+            self.add_at_head(val)
+            return
+        elif index > self.length:
+            return
+        new_element = Element(val)
+        counter = 0
+        if index <= self.length // 2:
+            current_element = self.head
+            while counter != index:
+                current_element = current_element.next
+                counter += 1
+        else:
+            current_element = self.tail
+            while counter != (self.length - index - 1):
+                current_element = current_element.prev
+                counter += 1
+        new_element.next = current_element
+        new_element.prev = current_element.prev
+        current_element.prev.next = new_element
+        current_element.prev = new_element
+        self.length += 1
+
+    def get(self, index):
+        if index >= self.length:
+            return 'Index out of range'
+        counter = 0
+        if index <= self.length // 2:
+            current_element = self.head
+            while counter != index:
+                current_element = current_element.next
+                counter += 1
+        else:
+            current_element = self.tail
+            while counter != (self.length - 1 - index):
+                current_element = current_element.prev
+                counter += 1
+        return current_element.val
+
+    def delete_at_index(self, index):
+        if index >= self.length:
+            return
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return
+        if index == 0:
+            new_head = self.head.next
+            new_head.prev = None
+            self.head = new_head
+            self.length -= 1
+            return
+        if index == self.length - 1:
+            new_tail = self.tail.prev
+            new_tail_next = None # noqa
+            self.tail = new_tail
+            self.length -= 1
+            return
+        counter = 0
+        if index <= self.length // 2:
+            current_element = self.head
+            while counter != index:
+                current_element = current_element.next
+                counter += 1
+        else:
+            current_element = self.tail
+            while counter != (self.length - index - 1):
+                current_element = current_element.prev
+                counter += 1
+        current_element.prev.next, current_element.next.prev = \
+            current_element.next, current_element.prev
+        self.length -= 1
+
+
+def reverse_linked_list(linked_list):
+    reversed_linked_list = LinkedList()
+    length = linked_list.length
+    counter = 0
+    current_element = linked_list.tail
+    while counter != length:
+        reversed_linked_list.add_at_tail(current_element.val)
+        current_element = current_element.prev
+        counter += 1
+    return reversed_linked_list
---
+++ tests/test_dll.py
@@ -0,0 +1,84 @@
+import pytest
+from linked_list.double_linked_list \
+    import Element, LinkedList, reverse_linked_list
+
+
+def test_double_linked_list():
+    linked_list = LinkedList()
+    assert linked_list.length == 0
+    linked_list.add_at_tail(1)
+    assert linked_list.length == 1
+    assert linked_list.tail.val == 1
+    assert linked_list.tail.next is None
+    linked_list.add_at_head(2)
+    assert linked_list.length == 2
+    assert linked_list.head.val == 2
+    assert linked_list.head.prev is None
+    linked_list.add_at_head(3)
+    assert linked_list.length == 3
+    assert linked_list.tail.val == 1
+    assert linked_list.head.val == 3
+    assert linked_list.head.next.val == 2
+    linked_list.add_at_tail(4)
+    assert linked_list.length == 4
+    assert linked_list.tail.val == 4
+    assert linked_list.tail.prev.val == 1
+    assert linked_list.head.val == 3
+    assert linked_list.get(0) == 3
+    assert linked_list.get(1) == 2
+    assert linked_list.get(2) == 1
+    assert linked_list.get(3) == 4
+    linked_list.add_at_index(4, 5)
+    assert linked_list.tail.val == 5
+    assert linked_list.length == 5
+    assert linked_list.tail.prev.val == 4
+    assert linked_list.head.val == 3
+    linked_list.add_at_index(0, -1)
+    assert linked_list.length == 6
+    assert linked_list.head.val == -1
+    assert linked_list.head.next.val == 3
+    linked_list.add_at_index(1, -2)
+    assert linked_list.length == 7
+    assert linked_list.head.val == -1
+    assert linked_list.tail.val == 5
+    assert linked_list.get(1) == -2
+    assert linked_list.get(2) == 3
+    linked_list.delete_at_index(0)
+    assert linked_list.length == 6
+    assert linked_list.head.val == -2
+    assert linked_list.get(0) == -2
+    linked_list.delete_at_index(5)
+    assert linked_list.length == 5
+    assert linked_list.tail.val == 4
+    assert linked_list.get(4) == 4
+    linked_list.delete_at_index(1)
+    assert linked_list.length == 4
+    assert linked_list.head.val == -2
+    assert linked_list.tail.val == 4
+    assert linked_list.get(1) == 2
+
+
+def test_reversed_linked_list():
+    linked_list = LinkedList()
+    linked_list.add_at_tail(1)
+    linked_list.add_at_tail(2)
+    linked_list.add_at_tail(3)
+    assert linked_list.get(0) == 1
+    assert linked_list.get(1) == 2
+    assert linked_list.get(2) == 3
+    reversed_linked_list = reverse_linked_list(linked_list)
+    assert reversed_linked_list.get(0) == 3
+    assert reversed_linked_list.get(1) == 2
+    assert reversed_linked_list.get(2) == 1
+
+
+def test_empty_linked_list():
+    linked_list = LinkedList()
+    assert linked_list.length == 0
+    assert linked_list.get(1) == 'Index out of range'
+    with pytest.raises(AttributeError):
+        assert linked_list.head.value is None
+    with pytest.raises(AttributeError):
+        assert linked_list.tail.value is None
+    linked_list.delete_at_index(1)
+    assert linked_list.length == 0
